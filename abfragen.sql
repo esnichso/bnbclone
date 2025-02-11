3515a9955a449bf06363a3d35073bb41252a72ca
esnichso <68183770+esnichso@users.noreply.github.com>
2025-02-11 09:42:32 +0100

    Update and rename abfragen.sql to datenbank/abfragen.sql

--- abfragen.sql
+++
@@ -1,5 +0,0 @@
--- Wohnungen auflisten
-SELECT * FROM Vermieter, Wohnungen WHERE Vermieter.VermieterID == Wohnungen.VermieterID;
-
---Buchungen auflisten
-SELECT Buchungen.BuchungsID, Mieter.name, Wohnungen.WID, Buchungen.Datum FROM Buchungen, Mieter, Wohnungen WHERE Buchungen.WohnungsID = Wohnungen.WID
