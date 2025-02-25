863451459a495109d93bb113aba6e29afae6b540
esnichso <esnichso@protonmail.com>
2025-02-25 16:13:46 +0100

    added search and connected gui and sql

--- gui.py
+++ gui.py
@@ -1,12 +1,14 @@
 from tkinter import * 
 from tkinter import ttk
 
+import sqlinterface
 
 
 class App:
-    def __init__(self):
+    def __init__(self, db):
         self.root = Tk()
         self.root.title("BnB Clone") # Titel setzen
+        self.database = sqlinterface.Database(db)
 
         self.loginpage = LoginPage(self.root, self)
         self.mieterpage = MieterPage(self.root, self)
@@ -80,13 +82,22 @@ class LoginPage:
 
     def anmelden(self, mieter):
         print(self.EmailField.get())
-        print("Anmelden als", "Mieter" if mieter else "Vermiter")
-        self.loginFrame.grid_forget()
-        self.firstFrame.grid()
-        self.total.grid_forget()
+        print("Anmelden als", "Mieter" if mieter else "Vermieter")
+
         if mieter:
-            self.app.mieterpage.total.grid()
-            self.app.mieterpage.startpage.grid()
+            res = self.app.database.select(f"SELECT * FROM Mieter WHERE Email == '{self.EmailField.get()}'")
+            if res == []:
+                print("User existiert nicht")
+                ttk.Label(self.loginFrame, text="falsche Email").grid()
+            else:
+                global mieterID
+                mieterID = res[0]
+                print(mieterID)
+                self.loginFrame.grid_forget()
+                self.firstFrame.grid()
+                self.total.grid_forget()
+                self.app.mieterpage.total.grid()
+                self.app.mieterpage.startpage.grid()
 
 
 class MieterPage:
@@ -95,9 +106,12 @@ class MieterPage:
         self.app = app
         self.total = ttk.Frame(self.master)
         
+        self.mainpage()
+        
+
+    def mainpage(self):
         self.startpage = ttk.Frame(self.total)
         self.startpage.grid()
-        self.suchPage = ttk.Frame(self.total)
 
         self.startpageBanner = ttk.Label(self.startpage,
                                 text="Eingeloggt als Mieter")
@@ -105,7 +119,7 @@ class MieterPage:
 
         self.startpageSucheButton = ttk.Button(self.startpage,
                                                text="Suche",
-                                               command= lambda: [self.startpage.grid_forget(), self.suchPage.grid()])
+                                               command= lambda: [self.startpage.grid_forget(), self.initsuchpage()])
         self.startpageSucheButton.grid()
 
         self.startpageAbmeldeButton = ttk.Button(self.startpage, 
@@ -114,9 +128,45 @@ class MieterPage:
                                      )
         self.startpageAbmeldeButton.grid()
 
+
+    def initsuchpage(self):
+        self.suchPage = ttk.Frame(self.total)
+        self.suchPage.grid()
+
         self.suchpageBanner = ttk.Label(self.suchPage,
                                         text="Suche")
-        self.suchpageBanner.grid()
+        self.suchpageBanner.grid(row=0, column=0)
+
+        self.suchpageStadtLabel = ttk.Label(self.suchPage, text="Stadt")
+        self.suchpageStadtLabel.grid(row=1, column=0)
+        self.suchpageStadt = StringVar()
+
+        StadtData = ["Any"] + self.app.database.select("SELECT Stadt from Wohnungen GROUP BY Stadt") #Städte abfragen
+        self.suchpageStadtAuswahl = ttk.OptionMenu(self.suchPage, self.suchpageStadt, *StadtData)
+        self.suchpageStadtAuswahl.grid(row=1, column = 1)
+        
+        self.suchpageLandLabel = ttk.Label(self.suchPage, text="Land")
+        self.suchpageLandLabel.grid(row=2, column=0)
+        self.suchpageLand = StringVar()
+
+        LandData = ["Any"] + self.app.database.select("SELECT Land from Wohnungen GROUP BY Land") # Länder Abfrage
+
+        self.suchpageLandAuswahl = ttk.OptionMenu(self.suchPage, self.suchpageLand, *LandData)
+        self.suchpageLandAuswahl.grid(row=2, column = 1)
+
+        self.suchpageBettenLabel = ttk.Label(self.suchPage, text="Betten")
+        self.suchpageBettenLabel.grid(row=3, column=0)
+        self.suchpageBettenAnzahl = ttk.Entry(self.suchPage)
+        self.suchpageBettenAnzahl.grid(row=3, column=1)
+
+        self.suchPageSubmitButton = ttk.Button(self.suchPage, 
+                                     text="Suche", 
+                                     command= lambda: self.suche()
+                                     )
+        self.suchPageSubmitButton.grid()
+        
+        self.suchPageResults = ttk.Frame(self.suchPage)
+        self.suchPageResults.grid()
 
         self.suchPageBackButton = ttk.Button(self.suchPage, 
                                      text="Zurück", 
@@ -124,5 +174,42 @@ class MieterPage:
                                      )
         self.suchPageBackButton.grid()
 
-app = App()
+
+    def suche(self):
+        self.suchPageResults.grid_forget()
+        self.suchPageResults = ttk.Frame(self.suchPage)
+        self.suchPageResults.grid()
+
+        stadt = self.suchpageStadt.get()
+        land = self.suchpageLand.get()
+        betten = self.suchpageBettenAnzahl.get()
+
+        print("Stadt:", stadt, "\nLand:", land, "\nBetten:", betten) 
+
+        res = self.app.database.listWohnungen(land=land, stadt=stadt, betten=betten)
+        print(res)
+        counter = 0
+        resgui = []
+        for i in res:
+            resgui.append((ttk.Label(self.suchPageResults, text=f"Apartment Nr. {i[3]}\n{i[4]}, {i[5]}\nBetten: {i[6]}\n\n"), 
+                             ttk.Button(self.suchPageResults, text="Buchen", command= lambda c= i: Buchungsfenster(self.master, self.app, c[3]))))
+            resgui[counter][0].grid(row=counter, column=0)
+            resgui[counter][1].grid(row=counter, column = 1)
+            counter+=1
+
+class Buchungsfenster(Toplevel):
+    def __init__(self, master, app, wid):
+        super().__init__(master=master)
+        self.app = app
+        self.title("Buchung")
+        self.geometry("200x200")
+        ttk.Label(self, text=f"Buchung - {wid}").grid()
+        d = [i for i in self.app.database.selectWohnung(wid)[0]]
+        print(d)
+        ttk.Label(self, text=f"Apartment Nr. {d[0]}\n{d[2]}, {d[1]}\nBetten: {d[3]}\nVermietet von {d[4]}").grid()
+        datum = ttk.Entry(self)
+        datum.grid()
+        ttk.Button(self, text="Buchen", command= lambda: print(datum.get())).grid()
+
+app = App("datenbank/db.sqlite")
 app.run()
--- sqlinterface.py
+++ sqlinterface.py
@@ -13,9 +13,9 @@ class Database:
     
     def listWohnungen(self, land="", stadt="", betten=""):
         query = "SELECT * FROM Vermieter, Wohnungen WHERE Vermieter.VermieterID == Wohnungen.VermieterID"
-        if land != "":
+        if land != "Any":
             query += " AND Wohnungen.Land == '" + land + "'"
-        if stadt != "":
+        if stadt != "Any":
             query += " AND Wohnungen.Stadt == '" + stadt + "'" 
         if betten != "":
             query += " AND Wohnungen.betten >= " + betten + ""
@@ -25,29 +25,34 @@ class Database:
         return self.cursor.execute("SELECT Buchungen.BuchungsID, Mieter.MieterID, Mieter.name, Wohnungen.WID, Buchungen.Datum FROM Buchungen, Mieter, Wohnungen WHERE Buchungen.WohnungsID = Wohnungen.WID").fetchall()
 
     def select(self, query):
-       return self.cursor.execute(query).fetchall()
-    
+       res = self.cursor.execute(query).fetchall()
+       return [i[0] for i in res]
     
     def addVermieter(self, data=(str, str)):
-        self.cursor.execute(f"INSERT INTO \"main\".\"Vermieter\" (\"Name\", \"Email\") VALUES (\'{data["name"]}\', \'{data["email"]}\')")
+        self.cursor.execute(f"INSERT INTO \"main\".\"Vermieter\" (\"Name\", \"Email\") VALUES (\'{data['name']}\', \'{data['email']}\')")
         self.connection.commit()
 
     def addMieter(self, data=(str, str)):
-        self.cursor.execute(f"INSERT INTO \"main\".\"Mieter\" (\"Name\", \"Email\") VALUES (\'{data["name"]}\', \'{data["email"]}\')")
+        self.cursor.execute(f"INSERT INTO \"main\".\"Mieter\" (\"Name\", \"Email\") VALUES (\'{data['name']}\', \'{data['email']}\')")
         self.connection.commit()
 
     def addWohnung(self, data):
-        self.cursor.execute(f"INSERT INTO \"main\".\"Wohnungen\" (\"Stadt\", \"Land\", \"Betten\", \"VermieterID\") VALUES (\'{data["stadt"]}\', \'{data["land"]}\', \'{data["betten"]}\', \'{data["vid"]}\')")
+        self.cursor.execute(f"INSERT INTO \"main\".\"Wohnungen\" (\"Stadt\", \"Land\", \"Betten\", \"VermieterID\") VALUES (\'{data['stadt']}\', \'{data['land']}\', \'{data['betten']}\', \'{data['vid']}\')")
         self.connection.commit()
     
     def addBuchung(self, data):
-        self.cursor.execute(f"INSERT INTO \"main\".\"Buchungen\" (\"WohnungsID\", \"MieterID\", \"Datum\") VALUES (\'{data["wid"]}\', \'{data["mid"]}\', \'{data["datum"]}\')")
+        self.cursor.execute(f"INSERT INTO \"main\".\"Buchungen\" (\"WohnungsID\", \"MieterID\", \"Datum\") VALUES (\'{data['wid']}\', \'{data['mid']}\', \'{data['datum']}\')")
         self.connection.commit()
 
-        
+    def selectWohnung(self, wid):
+        res = self.cursor.execute(f"SELECT Wohnungen.WID, Wohnungen.Land, Wohnungen.Stadt, Wohnungen.Betten, Vermieter.Name FROM Vermieter, Wohnungen WHERE Vermieter.VermieterID == Wohnungen.VermieterID AND WID == '{wid}'").fetchall()
+        return res
 
-db = Database("datenbank/db.sqlite")
-res = db.listWohnungen(betten="5")
-for i in res:
-    print(i)
+   
+if __name__ == "__main__":
+    db = Database("datenbank/db.sqlite")
+    res = db.select("SELECT Stadt from Wohnungen GROUP BY Stadt")
+    print(res)
+    for i in res:
+        print(i)
     
